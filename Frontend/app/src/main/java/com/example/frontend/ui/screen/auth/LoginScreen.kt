5cb789e789a2f4d6b2fcf9fff483a4628356546b
Pegasuarr <heng525555@gmail.com>
2026-05-14 22:59:50 +0700

    create a structure that need to use

---
+++ Frontend/app/src/main/java/com/example/frontend/ui/screen/auth/LoginScreen.kt
@@ -0,0 +1,25 @@
+package com.example.frontend.ui.screen.auth
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .padding(12.dp)
+    ){
+
+    }
+}
+
+@Composable
+@Preview
+fun LoginPreview(){
+    LoginScreen()
+}
