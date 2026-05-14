1469b3382f64fd9a6d40cb4386a4ece3bf64b6d8
Pegasuarr <heng525555@gmail.com>
2026-05-14 22:59:58 +0700

    create a structure that need to use

--- Frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.frontend.ui.theme.FrontendTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,4 +44,9 @@ fun GreetingPreview() {
     FrontendTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun FrontendTheme(content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
 }
